e9072106b64705039ea980a8c413d0f07dafd998
Benson <hsiehph@uw.edu>
2022-02-19 15:17:20 -0800

    new config.sh

--- config.sh
+++ config.sh
@@ -4,3 +4,6 @@ module purge
 . /etc/profile.d/modules.sh
 module load modules modules-init modules-gs/prod modules-eichler
 module load picard/2.26.4
+module load minimap2/2.24
+module load samtools/1.9
+module load bedtools/2.29.2
