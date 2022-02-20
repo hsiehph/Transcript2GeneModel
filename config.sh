5c90865e9f7bd1e877cfdfa2f7db758eae1053d7
Benson <hsiehph@uw.edu>
2022-02-19 16:10:18 -0800

    rm conda deactivate

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 unset PYTHONPATH
 # unsetting your current running env is important
-conda deactivate
 module purge
 . /etc/profile.d/modules.sh
 module load modules modules-init modules-gs/prod modules-eichler
